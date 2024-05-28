30f124aadfa28e9ff59e74eebf36c0c266e420a1
zorbeyyavas <yavaszorbey@gmail.com>
2024-05-29 02:01:49 +0300

    Add files via upload

---
+++ c dili math.c
@@ -0,0 +1,21 @@
+
+int main(){
+
+//#include <math.h> kütüphensi ile matematiksel fonksyonlar
+
+
+double a = sqrt(16); //verilen sayının karekkünü alır 
+double b = pow(2,3); //üssünü almadır 2nin 3. kuvvetini bulucak
+int c = round(3.25); //yuvarlama demektir bir sayıyı yakın olduğu tarafa yuvarlayacaktır
+int d = ceil(3.01); //üste yuvarla demektir 3.99 da grsek 4e yuvarlıycak 3.01 de 
+int e = floor(3.99); //altına yuvarlıycak 
+double u = fabs(-124); //mutlak değer almadır
+double y = log(5); //bir sayının logaritmasını almaya yarar
+double s = sin(45); //bir sayının sinüsünü alma
+double g = cos(45); // bir sayının cosinüsünü alma
+double z = tan(45); //bir sayının tanjantını alma
+
+printf("\n %lf",s);
+printf("\n %lf",g);
+printf("\n %lf",z); 
+}
